0556670a56bdd8bf9e9ab2bfe45b31cca32c85ef
Florian Reinhard <florian.reinhard@epfl.ch>
2014-02-05 19:35:45 +0100

    SocketHandler init and utils

--- SocketHandler.py
+++ SocketHandler.py
@@ -14,27 +14,45 @@ import select       # for poll (cross-platform I/O wait)
 class SocketHandler:
 
     def __init__(self, sockets = None, msgtypes = None):
-        self.sockets = sockets
-        self.msgtypes = msgtypes    # this should be a list of tuples (type, cb)
+        self.sockets = set(sockets)
+        self.msgtypes = dict(msgtypes)  # input should be {UID:CallBack}
+        self.poll = select.poll()
+        for s in self.sockets:
+            self.poll.register(s.fileno(), socket.POLLIN | socket.POLLPRI)
 
 
-    def addSocket(self, socket):
+
+    def addSocket(self, sock):
         # add socket to list and register to poll
+        if sock not in self.sockets:
+            self.sockets.add(sock)
+            self.poll.register(sock.fileno(), socket.POLLIN | socket.POLLPRI)
 
 
-    def rmSocket(self, socket):
+    def rmSocket(self, sock):
         # remove socket from list and unregister from poll
+        try:
+            self.sockets.remove(sock)
+            self.poll.unregister(sock.fileno())
+        finally:
+            pass
 
 
     def addMsgType(self, msgtype, callback):
         # add message type (UID) and the corresponding callback function pointer
+        self.msgtypes[msgtype] = callback
 
 
     def rmMsgType(self, msgtype):
         # remove message type (UID)
+        try:
+            del self.msgtypes[msgtype]
+        finally:
+            pass
 
 
     def handle(self):
         # poll sockets, deserialize, callback
+        pass
 
 
